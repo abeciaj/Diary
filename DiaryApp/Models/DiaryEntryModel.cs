d20ee91c2482d48861704af5eb97b8b02c2de091
abeciaj <abeciaj23@gmail.com>
2025-08-27 05:07:15 +1000

    init

---
+++ DiaryApp/Models/DiaryEntryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntryModel
+    {
+        [Key]
+        public int EntryId { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
