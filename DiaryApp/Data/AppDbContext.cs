c5915054ef1721b963d7d290e1591a362695acce
abeciaj <abeciaj23@gmail.com>
2025-09-03 06:08:25 +1000

    add diary controller

--- DiaryApp/Data/AppDbContext.cs
+++ DiaryApp/Data/AppDbContext.cs
@@ -10,5 +10,28 @@ namespace DiaryApp.Data
         }
 
         public DbSet<Models.DiaryEntryModel> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Models.DiaryEntryModel>()
+                .HasData(
+                    new Models.DiaryEntryModel
+                    {
+                        EntryId = 1,
+                        Title = "Welcome to Your Diary",
+                        Content = "This is your first diary entry. Feel free to edit or delete it.",
+                        CreatedAt = DateTime.Now
+                    },
+                    new Models.DiaryEntryModel
+                    {
+                        EntryId = 2,
+                        Title = "Getting Started",
+                        Content = "Start writing your thoughts and experiences here. Your diary is a safe space.",
+                        CreatedAt = DateTime.Now
+                    }
+                );
+        }
     }
 }
